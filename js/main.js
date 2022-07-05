110be2d03473bc84c0989e2159272a4fae1efc4a
Marta Iachini <iachinimarta@gmail.com>
2022-07-05 15:06:01 +0200

    Aggiunti commenti per spiegare le modifiche apportate

--- js/main.js
+++ js/main.js
@@ -6,29 +6,29 @@ const costo1Chilometro = 0.21;
 
 let prezzoBiglietto = costo1Chilometro * numeroChilometri;
 
-let messaggio = '';
+let messaggio = ''; //definisco così il messaggio in modo che possa essere modificato nel corso del codice
 
 const sconto20 = ((20 / 100) * prezzoBiglietto);
 
 const sconto40 = ((40 / 100) * prezzoBiglietto);
 
-let prezzo20 = (prezzoBiglietto - sconto20);
+let prezzo20 = (prezzoBiglietto - sconto20); //spostate le variabili che erano dentro if 
 
-let prezzo40 = (prezzoBiglietto - sconto40);
+let prezzo40 = (prezzoBiglietto - sconto40); //spostate le variabili che erano dentro if 
 
 console.log('Chilometri da percorrere: ' + numeroChilometri);
 console.log('Il passeggero ha: ' + etaPasseggero);
 console.log('Prezzo biglietto intero: ' + prezzoBiglietto);
 
 if (etaPasseggero < 18) {
-    prezzoBiglietto = prezzo20;
+    prezzoBiglietto = prezzo20; //meglio non definire delle variabili all'interno delle if
     messaggio = 'Prezzo scontato per i minorenni: ' + prezzo20.toFixed(2) + '€';
 } else if (etaPasseggero > 65) {
-    prezzoBiglietto = prezzo40;
+    prezzoBiglietto = prezzo40; //meglio non definire delle variabili all'interno delle if
     messaggio = 'Prezzo scontato per over 65: ' + prezzo40.toFixed(2) + '€';
 } else {
     messaggio = 'Prezzo del biglietto: ' + prezzoBiglietto.toFixed(2) + '€';
 }
 
-alert(messaggio);
+alert(messaggio); //definisco così il messaggio in modo che possa essere modificato nel corso del codice
 
