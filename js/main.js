68964106c5c3f7f37ca5ecb277abb127197671b1
Marta Iachini <iachinimarta@gmail.com>
2022-07-05 15:03:53 +0200

    Altre correzioni

--- js/main.js
+++ js/main.js
@@ -12,19 +12,23 @@ const sconto20 = ((20 / 100) * prezzoBiglietto);
 
 const sconto40 = ((40 / 100) * prezzoBiglietto);
 
+let prezzo20 = (prezzoBiglietto - sconto20);
+
+let prezzo40 = (prezzoBiglietto - sconto40);
+
 console.log('Chilometri da percorrere: ' + numeroChilometri);
 console.log('Il passeggero ha: ' + etaPasseggero);
 console.log('Prezzo biglietto intero: ' + prezzoBiglietto);
 
 if (etaPasseggero < 18) {
-    let prezzo20 = (prezzoBiglietto - sconto20);
+    prezzoBiglietto = prezzo20;
     messaggio = 'Prezzo scontato per i minorenni: ' + prezzo20.toFixed(2) + '€';
 } else if (etaPasseggero > 65) {
-    let prezzo40 = (prezzoBiglietto - sconto40);
+    prezzoBiglietto = prezzo40;
     messaggio = 'Prezzo scontato per over 65: ' + prezzo40.toFixed(2) + '€';
 } else {
     messaggio = 'Prezzo del biglietto: ' + prezzoBiglietto.toFixed(2) + '€';
 }
 
-alert (messaggio);
+alert(messaggio);
 
